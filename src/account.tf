362d3e5d41ea9bb5b725110e9c70de82d0a41ff3
shirakiya <shirakiya.pv@gmail.com>
2026-06-21 01:52:05 +0900

    remove 2 accounts

--- src/account.tf
+++ src/account.tf
@@ -3,20 +3,3 @@ resource "aws_organizations_account" "shirakiya" {
   email     = var.shirakiya_email
   parent_id = aws_organizations_organizational_unit.shirakiya_private.id
 }
-
-resource "aws_organizations_account" "torii" {
-  name      = "torii"
-  email     = var.torii_email
-  parent_id = aws_organizations_organizational_unit.shirakiya_private.id
-}
-
-resource "aws_organizations_account" "akashi_button" {
-  name      = "akashi-button"
-  email     = var.akashi_button_email
-  parent_id = aws_organizations_organizational_unit.shirakiya_private.id
-  role_name = "OrganizationAccountAccessRole"
-
-  lifecycle {
-    ignore_changes = [role_name]
-  }
-}
